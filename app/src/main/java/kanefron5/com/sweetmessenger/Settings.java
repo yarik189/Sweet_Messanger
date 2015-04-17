e9a41342e14ee683c6a00071781a12439a2ba193
kanefron5 <kanerfon5@gmail.com>
2015-04-17 19:44:45 +0300

    провел работу над орфографией

--- app/src/main/java/kanefron5/com/sweetmessenger/Settings.java
+++ app/src/main/java/kanefron5/com/sweetmessenger/Settings.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.view.View;
 
 import kanefron5.com.sweetmessanger.R;
 
@@ -16,25 +17,27 @@ public class Settings extends PreferenceFragment {
 
     private Settings t;
 
-      public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.pref);
 
-          /** Preference version = (Preference) findPreference("Версия");
-        version
-                .setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-                    public boolean onPreferenceClick(Preference p) {
-                        Intent i = new Intent(Settings.this, MainActivity.class);
-                        startActivity(i);
+        /** Preference version = (Preference) findPreference("Версия");
+         version
+         .setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+         public boolean onPreferenceClick(Preference p) {
+         Intent i = new Intent(Settings.this, MainActivity.class);
+         startActivity(i);
 
-                        return true;
-                    }
+         return true;
+         }
 
-                }); */
+         }); */
+
+
+
 
     }
-
-
 }
+
 
 
