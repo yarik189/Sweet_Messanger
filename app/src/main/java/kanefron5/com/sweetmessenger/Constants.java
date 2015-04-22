701188d845005d2701ab066c7e30fb56c99add52
kanefron5 <kanerfon5@gmail.com>
2015-04-22 14:51:53 +0300

    возможность добавления поста на стену

---
+++ app/src/main/java/kanefron5/com/sweetmessenger/Constants.java
@@ -0,0 +1,7 @@
+package kanefron5.com.sweetmessenger;
+
+public class Constants {
+    //Этот ID предназначен только для примера. Пожалуйста замение его ID своего приложения.
+    public static String API_ID="4856309";
+
+}
--- app/src/main/java/kanefron5/com/sweetmessenger/LoginActivity.java
+++ app/src/main/java/kanefron5/com/sweetmessenger/LoginActivity.java
@@ -1,125 +1,69 @@
 package kanefron5.com.sweetmessenger;
 
+import com.perm.kate.api.Auth;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-import com.vk.sdk.VKAccessToken;
-import com.vk.sdk.VKScope;
-import com.vk.sdk.VKSdk;
-import com.vk.sdk.VKSdkListener;
-import com.vk.sdk.VKUIHelper;
-import com.vk.sdk.api.VKError;
-import com.vk.sdk.api.VKRequest;
-
-
-
-/**
- * Created by Роман on 15.04.2015.
- */
 public class LoginActivity extends Activity {
+    private static final String TAG = "Kate.LoginActivity";
 
-    private static final String VK_APP_ID = "4856309";
-    private final VKSdkListener sdkListener = new VKSdkListener() {
-
-        @Override
-        public void onAcceptUserToken(VKAccessToken token) {
-            Log.d("VkDemoApp", "onAcceptUserToken " + token);
-            startLoading();
-        }
-
-        @Override
-        public void onReceiveNewToken(VKAccessToken newToken) {
-            Log.d("VkDemoApp", "onReceiveNewToken " + newToken);
-            startLoading();
-        }
-
-        @Override
-        public void onRenewAccessToken(VKAccessToken token) {
-            Log.d("VkDemoApp", "onRenewAccessToken " + token);
-            startLoading();
-        }
-
-        @Override
-        public void onCaptchaError(VKError captchaError) {
-            Log.d("VkDemoApp", "onCaptchaError " + captchaError);
-        }
-
-        @Override
-        public void onTokenExpired(VKAccessToken expiredToken) {
-            Log.d("VkDemoApp", "onTokenExpired " + expiredToken);
-        }
-
-        @Override
-        public void onAccessDenied(VKError authorizationError) {
-            Log.d("VkDemoApp", "onAccessDenied " + authorizationError);
-        }
-
-    };
-
-    private VKRequest currentRequest;
-    private Button loginButton;
-
+    WebView webview;
+    
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_login);
-
-
-        VKSdk.initialize(sdkListener, VK_APP_ID);
-
-        VKUIHelper.onCreate(this);
-
-        loginButton = (Button) findViewById(R.id.login_button);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                VKSdk.authorize(VKScope.FRIENDS, VKScope.MESSAGES);
+        setContentView(R.layout.login);
+        
+        webview = (WebView) findViewById(R.id.vkontakteview);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.clearCache(true);
+        
+        //Чтобы получать уведомления об окончании загрузки страницы
+        webview.setWebViewClient(new VkontakteWebViewClient());
+                
+        //otherwise CookieManager will fall with java.lang.IllegalStateException: CookieSyncManager::createInstance() needs to be called before CookieSyncManager::getInstance()
+        CookieSyncManager.createInstance(this);
+        
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.removeAllCookie();
+        
+        String url=Auth.getUrl(Constants.API_ID, Auth.getSettings());
+        webview.loadUrl(url);
+    }
+    
+    class VkontakteWebViewClient extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            parseUrl(url);
+        }
+    }
+    
+    private void parseUrl(String url) {
+        try {
+            if(url==null)
+                return;
+            Log.i(TAG, "url="+url);
+            if(url.startsWith(Auth.redirect_url))
+            {
+                if(!url.contains("error=")){
+                    String[] auth=Auth.parseRedirectUrl(url);
+                    Intent intent=new Intent();
+                    intent.putExtra("token", auth[0]);
+                    intent.putExtra("user_id", Long.parseLong(auth[1]));
+                    setResult(Activity.RESULT_OK, intent);
+                }
+                finish();
             }
-        });
-
-        if (VKSdk.wakeUpSession()) {
-            startLoading();
-        } else {
-            loginButton.setVisibility(View.VISIBLE);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        VKUIHelper.onResume(this);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        VKUIHelper.onActivityResult(this, requestCode, resultCode, data);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        VKUIHelper.onDestroy(this);
-        if (currentRequest != null) {
-            currentRequest.cancel();
-        }
-
-    }
-
-    private void startLoading() {
-        loginButton.setVisibility(View.GONE);
-        if (currentRequest != null) {
-            currentRequest.cancel();
-        }
-
-Intent i = new Intent(LoginActivity.this, MainActivity.class);
-        startActivity(i);
     }
 }
